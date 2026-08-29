9ebb8f4e77d275c2136e0f859924d15c241d5d97
share121 <me@s121.top>
2026-08-29 14:24:29 +0800

    feat: 简易实现
    Signed-off-by: share121 <me@s121.top>

--- build.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    let mut res = winres::WindowsResource::new();
-    res.set_icon("app_icon.ico");
-    res.set("FileDescription", "洛雪音乐数据映射助手");
-    res.set("ProductName", "LX Music Helper");
-    res.set("OriginalFilename", "lx-helper.exe");
-    res.set("LegalCopyright", "Copyright (c) 2024");
-    res.compile().unwrap();
-}
--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,83 @@
-#![windows_subsystem = "windows"]
-
 use anyhow::Context;
 use fs_err as fs;
 use std::env;
 use std::path::Path;
-use std::path::PathBuf;
 use std::process::Command;
 
 fn main() -> anyhow::Result<()> {
-    if let Err(e) = link() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 4 {
+        println!("用法: helper.exe <target_exe_path> <src_dir> <dst_dir>");
+        return Ok(());
+    }
+
+    let target_exe_path = &args[1];
+    let src_dir = &args[2];
+    let dst_dir = &args[3];
+
+    let self_exe = env::current_exe()?;
+    let self_dir = self_exe.parent().context("无法获取程序运行目录")?;
+
+    if let Err(e) = link(src_dir, dst_dir) {
         eprintln!("连接目录失败: {e:?}");
     }
-    let exe_path = env::current_exe()?;
-    let lx_exe_path = exe_path.with_file_name("lx-music-desktop.exe");
-    if lx_exe_path.exists() {
-        let mut command = Command::new(lx_exe_path);
-        if let Some(parent) = exe_path.parent() {
-            command.current_dir(parent);
+    let target_exe_path = self_dir.join(target_exe_path);
+    if fs::exists(&target_exe_path).unwrap_or(false) {
+        let mut command = Command::new(&target_exe_path);
+        if let Some(exe_dir) = target_exe_path.parent() {
+            command.current_dir(exe_dir);
         }
-        command.spawn()?;
-        println!("程序已启动，助手即将退出。");
+        if let Err(e) = command.spawn() {
+            eprintln!("无法启动主程序: {e:?}");
+        }
     } else {
         eprintln!(
-            "错误：找不到主程序 {}，请确认助手放在软件根目录下。",
-            lx_exe_path.display()
+            "错误：找不到主程序 {}\n请确认助手放在软件根目录下。",
+            target_exe_path.display()
         );
     }
     Ok(())
 }
 
-fn link() -> anyhow::Result<()> {
-    let appdata = env::var("APPDATA").context("无法获取 APPDATA 变量")?;
-    let link_path = PathBuf::from(appdata).join("lx-music-desktop");
-    let target_path = env::current_exe()
-        .context("无法获取自身路径")?
-        .with_file_name("lx-data");
-    if !target_path.try_exists()? {
-        fs::create_dir_all(&target_path)?;
-        println!("创建目标目录: {}", target_path.display());
+fn link(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> anyhow::Result<()> {
+    let src = src.as_ref();
+    let dst = dst.as_ref();
+    if !fs::exists(dst).unwrap_or(false) {
+        fs::create_dir_all(dst).context("无法创建目标存储目录")?;
     }
-    if link_path.try_exists()? {
-        if link_path.is_symlink() {
-            println!("检测到已映射的联接，正在更新映射关系...");
-            fs::remove_dir(&link_path)?;
-        } else if link_path.is_dir() {
-            println!("检测到旧数据文件夹，正在迁移...");
-            copy_dir_all(&link_path, &target_path)?;
-            fs::remove_dir_all(&link_path)?;
-            println!("迁移完成。");
+    if fs::exists(src).unwrap_or(false) {
+        let metadata = fs::symlink_metadata(src)?;
+        if metadata.is_symlink() || junction::exists(src).unwrap_or(false) {
+            fs::remove_dir(src).context("无法移除旧的联接点")?;
+        } else if metadata.is_dir() {
+            println!("正在迁移数据...");
+            move_dir(src, dst).context("数据迁移失败")?;
+            println!("迁移完成");
         } else {
-            fs::remove_file(&link_path)?;
+            fs::remove_file(src).context("路径被文件占用，且无法删除")?;
         }
     }
-    junction::create(&target_path, &link_path)?;
-    println!(
-        "成功将 {} 映射到 {}",
-        link_path.display(),
-        target_path.display()
-    );
+    junction::create(dst, src).context("建立目录联接失败")?;
+    println!("已成功建立关联: {} -> {}", src.display(), dst.display());
     Ok(())
 }
 
-/// 递归拷贝文件夹内容的辅助函数
-fn copy_dir_all(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> std::io::Result<()> {
-    fs::create_dir_all(&dst)?;
-    for entry in fs::read_dir(src.as_ref())? {
-        let entry = entry?;
-        let file_type = entry.file_type()?;
-        if file_type.is_dir() {
-            copy_dir_all(entry.path(), dst.as_ref().join(entry.file_name()))?;
-        } else {
-            fs::copy(entry.path(), dst.as_ref().join(entry.file_name()))?;
-        }
+/// 调用系统 Robocopy 迁移数据
+fn move_dir(src: &Path, dst: &Path) -> anyhow::Result<()> {
+    #[allow(clippy::unreadable_literal)]
+    let status = Command::new("robocopy")
+        .arg(src)
+        .arg(dst)
+        .arg("/E") // 递归拷贝，包含子目录
+        .arg("/MOVE") // 移动文件（成功后会自动删除源文件和目录）
+        .arg("/XJ") // 排除联接点，防止死循环
+        .arg("/MT:8") // 8线程开启
+        .arg("/R:3") // 失败重试3次
+        .arg("/W:1") // 间隔1秒
+        .status()?;
+    if status.code().unwrap_or(8) < 8 {
+        Ok(())
+    } else {
+        anyhow::bail!("Robocopy 迁移任务失败，退出码: {:?}", status.code())
     }
-    Ok(())
 }
